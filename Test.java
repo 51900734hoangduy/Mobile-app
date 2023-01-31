4a5a64359c6b53e93ce0bd7cdf64ce5c8baf4d3d
51900734hoangduy <97268938+51900734hoangduy@users.noreply.github.com>
2023-01-31 08:36:36 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Cau1 {
   public static void main(String[] args) {
-    System.out.println("ban la nhat ");
+    System.out.println("ban la nhat sao ky v");
     
   }
 }
